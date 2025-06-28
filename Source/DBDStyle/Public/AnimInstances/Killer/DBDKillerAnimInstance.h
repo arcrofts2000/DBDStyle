1b80972b36f6fd021b4d59b23b04969911e37130
arcrofts2000 <arcrofts2000@gmail.com>
2025-06-28 23:56:13 +0100

    Created Killer ABP

---
+++ Source/DBDStyle/Public/AnimInstances/Killer/DBDKillerAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstances/DBDCharacterAnimInstance.h"
+#include "DBDKillerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DBDSTYLE_API UDBDKillerAnimInstance : public UDBDCharacterAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DBDStyle/Public/AnimInstances/Killer/DBDKillerLinkedAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstances/DBDBaseAnimInstance.h"
+#include "DBDKillerLinkedAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DBDSTYLE_API UDBDKillerLinkedAnimInstance : public UDBDBaseAnimInstance
+{
+	GENERATED_BODY()
+	
+};
